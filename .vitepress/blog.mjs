7d20e6203ed15b55ecc64221a4e5fefafe3a1237
mPandaer <liwenhaolx@gmail.com>
2024-10-08 22:05:00 +0800

    改造全部文章页

--- .vitepress/blog.mjs
+++ .vitepress/blog.mjs
@@ -3,19 +3,24 @@ import path from 'path'
 
 const itemMd = [`---\noutline: [2,5]\n---\n# 全部文章 \n`];
 
+const colorFlags = ['NOTE','TIP','IMPORTANT','WARNING','CAUTION']
+let index = 0;
+
 export function getAllFiles(dir) {
     let files = fs.readdirSync(dir, { withFileTypes: true });
     files = files.filter(file => file.name !== "images")
     files.forEach(file => {
         if (file.isDirectory()) {
             let dirPath =  path.join(dir, file.name)
-            itemMd.push(`## ${file.name}\n`);
+            const ranColorFlag = colorFlags[(index++) % colorFlags.length];
+            itemMd.push(`> [!${ranColorFlag}] # ${file.name}\n`);
             getAllFiles(dirPath);
+            itemMd.push(`\n`);
         }else if (!file.name.includes("index")) {
             let fullPath = path.join(dir, file.name);
             fullPath = fullPath.replaceAll(path.sep,"/").replace(path.extname(file.name),"")
             let displayName = path.basename(file.name).replace(path.extname(file.name),"");
-            let mdStr = `- [${displayName}](/${encodeURIComponent(fullPath)})\n`;
+            let mdStr = `> - [${displayName}](/${encodeURIComponent(fullPath)})\n`;
             itemMd.push(mdStr);
         }
     })
